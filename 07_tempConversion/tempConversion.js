b753feb142a05a51aa4c1ec83b6f62d3f8c783ef
Hajji Ramos <leo_pips@yahoo.com>
2024-02-28 19:37:35 -0600

    finished all the assigned exercises"

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) * 5/9;
+  let tempCel = parseFloat(celsius.toFixed(1));
+  return tempCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 9/5 + 32;
+  let tempFah = parseFloat(fahrenheit.toFixed(1));
+  return tempFah;
 };
 
 // Do not edit below this line
